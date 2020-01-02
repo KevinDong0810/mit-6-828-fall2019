cfca90f35b475baa691d7f5a9afd95a6bc0d8806
Ke DONG <kedong0810@gmail.com>
2020-01-01 21:06:25 -0500

    finish uthread

--- user/uthread.c
+++ user/uthread.c
@@ -10,8 +10,30 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+// Saved registers for kernel context switches.
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
+
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
+  struct context context;
   int        state;             /* FREE, RUNNING, RUNNABLE */
 };
 struct thread all_thread[MAX_THREAD];
@@ -61,6 +83,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)&(t->context), (uint64)&(next_thread->context));
   } else
     next_thread = 0;
 }
@@ -75,6 +98,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)((t->stack) + STACK_SIZE);
 }
 
 void 
